05f2d2e31189e67b4ec963cccbdc257a1deb2e68
Dimosthenis Gkemitzoglou <128976781+DimosGk@users.noreply.github.com>
2023-09-24 17:34:26 +0300

    Add files via upload

---
+++ 10_HypotenuseCalculator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    double a;
+    double b;
+    double c;
+
+    std::cout << "Enter side A: " << '\n';
+    std::cin >> a;
+
+    std::cout << "Enter side B: " << '\n';
+    std::cin >> b;
+
+    a = pow(a, 2);
+    b = pow(b, 2);
+    c = sqrt(a + b);
+
+    std::cout << "Side C is: " << c;
+
+    return 0;
+}
